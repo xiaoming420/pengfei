5b0ff6df8d9edfb7f915babec4baea0993960b84
renkaiming <645236815@qq.com>
2018-03-24 12:07:06 +0800

    update

--- admindm-yourname/mod_common/tpl_index.php
+++ admindm-yourname/mod_common/tpl_index.php
@@ -3,14 +3,7 @@
  
 
 
-				欢迎使用DM企业建站系统，<br />
-				DM系统专注中小企业网站建设！<br />
-				DM系统开源，免费，无需授权！<br />
-				您值的拥有！ 
-				<br /><br />
-			 
-				使用DM建站，<a  style="color:blue;font-size:16px" target="_blank" href="<?php echo $userurl?>http://www.demososo.com/install.html">请先观看视频教程></a>
-				</span>
+				欢迎登录 后台管理系统！，<br />
 
 
 				</div>
